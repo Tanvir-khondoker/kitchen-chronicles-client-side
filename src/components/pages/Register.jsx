e68508851378936f945519538f0280c8573bdb0b
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2023-05-04 21:07:48 +0600

    password validation done using regular expression

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,99 +1,97 @@
-/* eslint-disable no-empty-pattern */
 /* eslint-disable no-unused-vars */
-import React, { useContext } from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-
-import { useState } from 'react';
-import { AuthContext } from '../../providers/AuthProvider';
-import { Result } from 'postcss';
+import React, { useContext, useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Register = () => {
+  const { createUser, profileUpdating } = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const [accepted, setAccepted] = useState(false);
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const email = form.email.value;
+    const password = form.password.value;
+      
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.{8,})/;
     
-    const {createUser,profileUpdating} = useContext(AuthContext);
-
-    const [accepted, setAccepted] = useState(false);
-
-    const handleRegister = event => {
-        event.preventDefault();
-        const form = event.target;
-        const name = form.name.value;
-        const photo = form.photo.value;
-        const email = form.email.value;
-        const password = form.password.value;
-
-        // console.log(name, photo, email, password)
-        createUser(email, password)
-        .then(Result =>{
-            const createdUser = Result.user;
-            console.log(createdUser);
-        })
-        .catch(error =>{
-            console.log(error);
-        })
-
-
-        profileUpdating(name, photo)
-        .then(() => {
-            // Profile updated!
-            // ...
-          }).catch((error) => {
-            console.log(error);
-          });
-        
+    if (!passwordRegex.test(password)) {
+      setError("Password must be at least 8 characters long, contain one capital letter, and one special character.");
+      return;
     }
 
-    const handleAccepted = event =>{
-        setAccepted(event.target.checked)
-    }
+    createUser(email, password)
+      .then((result) => {
+        const createdUser = result.user;
+        console.log(createdUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-    return (
-        <Container className='w-25 mx-auto'>
-            <h3>Please Register</h3>
-            <Form onSubmit={handleRegister}>
-                <Form.Group className="mb-3" >
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" name='name' placeholder="Your Name" required />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                    <Form.Label>Photo URL</Form.Label>
-                    <Form.Control type="text" name='photo' placeholder="Photo URL" required />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name='email' placeholder="Enter email" required />
-                </Form.Group>
+    profileUpdating(name, photo)
+      .then(() => {
+        // Profile updated!
+        // ...
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name='password' placeholder="Password" required />
-                </Form.Group>
+  const handleAccepted = (event) => {
+    setAccepted(event.target.checked);
+  };
 
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check
-                        onClick={handleAccepted}
-                        type="checkbox"
-                        name="accept"
-                        label={<>Accept <Link to="/terms-condition">Terms and Conditions</Link> </>} />
-                </Form.Group>
-                <Button variant="primary" disabled={!accepted} type="submit">
-                    Register
-                </Button>
-                <br />
-                <Form.Text className="text-secondary">
-                    Already Have an Account? <Link to="/login">Login</Link>
-                </Form.Text>
-                <Form.Text className="text-success">
+  return (
+    <Container className="w-25 mx-auto">
+      <h3>Please Register</h3>
+      <Form onSubmit={handleRegister}>
+        <Form.Group className="mb-3">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" name="name" placeholder="Your Name" required />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Photo URL</Form.Label>
+          <Form.Control type="text" name="photo" placeholder="Photo URL" required />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" name="email" placeholder="Enter email" required />
+        </Form.Group>
 
-                </Form.Text>
-                <Form.Text className="text-danger">
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" name="password" placeholder="Password" required />
+        </Form.Group>
+        <Form.Text className="text-danger">{error}</Form.Text>
 
-                </Form.Text>
-            </Form>
-        </Container>
-    );
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check
+            onClick={handleAccepted}
+            type="checkbox"
+            name="accept"
+            label={
+              <>
+                Accept <Link to="/terms-condition">Terms and Conditions</Link>{" "}
+              </>
+            }
+          />
+        </Form.Group>
+        <Button variant="primary" disabled={!accepted} type="submit">
+          Register
+        </Button>
+        <br />
+        <Form.Text className="text-secondary">
+          Already Have an Account? <Link to="/login">Login</Link>
+        </Form.Text>
+      </Form>
+    </Container>
+  );
 };
 
 export default Register;
-
-
